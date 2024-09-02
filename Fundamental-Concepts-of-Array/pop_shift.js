0c8e2d2e3c3836883a88d1e44eb1d7ca7e95ab41
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2024-09-02 22:06:06 +0600

    pop and shift

---
+++ Fundamental-Concepts-of-Array/pop_shift.js
@@ -0,0 +1,18 @@
+// REMOVING ELEMENTS
+
+//  pop()
+let fruits = ["Apple", "Banana", "Mango"];
+
+let lastFruit = fruits.pop();
+
+console.log(fruits); // Outputs: ["Apple", "Banana"]
+console.log(lastFruit); // Outputs: "Mango"
+
+
+// shift()
+let fruits = ["Apple", "Banana", "Mango"];
+
+let firstFruit = fruits.shift();
+
+console.log(fruits); // Outputs: ["Banana", "Mango"]
+console.log(firstFruit); // Outputs: "Apple"
